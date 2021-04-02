35da79f680662b11d40aa4e00f21a95876feb606
Oytizzo <oytizzo@gmail.com>
2021-04-02 22:37:16 +0600

    Add functionality to cart and chechout

--- src/ecom/models.py
+++ src/ecom/models.py
@@ -37,6 +37,18 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        cart_items = sum([item.quantity for item in orderitems])
+        return cart_items
 
 
 class OrderItem(models.Model):
@@ -44,6 +56,11 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
--- src/ecom/views.py
+++ src/ecom/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Product
+from .models import Product, Order
 
 def store(request):
     products = Product.objects.all()
@@ -11,12 +11,34 @@ def store(request):
 
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, 'ecom/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {
+        'items': items,
+        'order': order,
+    }
     return render(request, 'ecom/checkout.html', context)
 
 
