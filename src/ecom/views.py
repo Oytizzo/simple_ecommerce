a8cdc4c955639c29af83b1c388a70a35058e9357
Oytizzo <oytizzo@gmail.com>
2021-04-11 23:59:38 +0600

    Updated cart views cookie

--- src/ecom/views.py
+++ src/ecom/views.py
@@ -31,6 +31,11 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}
+        print('Cart:', cart)
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
