78e8729e3c466e1eb829212db534dca6c462cb14
Oytizzo <oytizzo@gmail.com>
2021-04-13 05:16:02 +0600

    Updated order process in views

--- src/ecom/views.py
+++ src/ecom/views.py
@@ -86,23 +86,52 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
+        
 
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['data'],
-                state=data['shipping']['data'],
-                zipcode=data['shipping']['data'],
-            )
+        
 
     else:
         print('User is not logged in!')
+
+        print('COOKIES:', request.COOKIES)
+        name = data['form']['name']
+        email = data['form']['email']
+
+        cookieData = cookieCart(request)
+        items = cookieData['items']
+
+        customer, created = Customer.objects.get_or_create(
+            email=email
+        )
+        customer.name = name
+        customer.save()
+
+        order = Order.objects.create(
+            customer=customer, complete=False
+        )
+
+        for item in items:
+            product = Product.objects.get(id=item['product']['id'])
+
+            orderItem = OrderItem.objects.create(
+                product=product, order=order, quantity=item['quantity']
+            )
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['data'],
+            state=data['shipping']['data'],
+            zipcode=data['shipping']['data'],
+        )
+
     return JsonResponse('Payment complete!', safe=False)
